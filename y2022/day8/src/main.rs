ace208de4e449d02079603f05da33e499dc6c022
crippa1337 <casdcasd5@gmail.com>
2023-05-30 23:13:26 +0200

    more work on day8

--- y2022/day8/src/main.rs
+++ y2022/day8/src/main.rs
@@ -1,10 +1,29 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 fn main() {
     solution::<true>();
+    // solution::<false>();
 }
 
 fn solution<const PART_1: bool>() {
-    let s = std::fs::read_to_string("test_input").unwrap();
-    let tree_rows: Vec<&str> = s.trim().split('\n').map(|x| x.trim()).collect();
-    println!("{tree_rows:?}");
-    let trees: Vec<Vec<u8>> = tree_rows.iter();
+    let f = File::open("test_input").unwrap();
+    let amount_of_lines = BufReader::new(&f).lines().count();
+
+    let f = File::open("test_input").unwrap();
+    let r = BufReader::new(f);
+
+    let mut visible_trees = 0;
+
+    for (idx, row) in r.lines().map(|r| r.unwrap()).enumerate() {
+        println!("row: {row}\nidx: {idx}");
+
+        if idx == 0 || idx == amount_of_lines - 1 {
+            visible_trees += row.len();
+        }
+    }
+
+    println!("{visible_trees}");
 }
